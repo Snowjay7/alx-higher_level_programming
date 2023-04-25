82f179762c652078838d3d0c2ea2dd07a28316b1
snowjay7 <ogwu.justina@gmail.com>
2023-04-25 08:21:15 -0700

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
 // script that computes the number of tasks completed by user id.
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
-      }
+const request = require('request');
+
+if (process.argv.length > 2) {
+  request(process.argv[2], (err, res, body) => {
+    const aggregate = {};
+
+    if (err) {
+      console.log(err);
     }
-    console.log(dash);
-  }
-});
+    JSON.parse(body).forEach(element => {
+      if (element.completed) {
+        if (!aggregate[element.userId]) {
+          aggregate[element.userId] = 0;
+        }
+        aggregate[element.userId]++;
+      }
+    });
+    console.log(aggregate);
+  });
+}
